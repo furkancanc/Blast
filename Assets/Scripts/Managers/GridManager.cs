72012371e820ed0f8cd98525b3c3ceb35c2516b9
furkancanc <fcc.ceylan@gmail.com>
2025-02-27 23:22:50 +0300

    Block Manager

--- Assets/Scripts/Managers/GridManager.cs
+++ Assets/Scripts/Managers/GridManager.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private Transform spawnedBlocksParent;
-    [SerializeField] private Sprite border;
+    [SerializeField] private SpriteRenderer border;
     private Grid grid;
-    
+
+    private void Start()
+    {
+        
+    }
+
     private void Initialize()
     {
-
+        
     }
 
     private void SetBorderSize()
